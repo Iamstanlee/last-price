604ab592287c70b4623fadb1d2d7a0230bfc8c4e
rukkiechovwe <rukkiechowe@gmail.com>
2021-09-15 17:25:44 +0100

    manual update

---
+++ src/pages/Dashboard/components/styles.js
@@ -0,0 +1,27 @@
+import styled from "styled-components"
+
+export const Card = styled.div`
+  width: 100%;
+  margin: 12px;
+  padding: 10px;
+  background: #fff;
+  border-radius: 4px;
+  box-shadow: 0px 1px 5px #5e5d5d2b;
+  }
+  @media (min-width: 420px) {
+   
+  }
+
+  
+`
+export const CardTitle = styled.p`
+  color: gray;
+  font-size: 14px;
+  margin-bottom: 0;
+`
+
+export const CardData = styled.p`
+  color: #242424;
+  font-size: 2rem;
+  margin-bottom: 0;
+`
